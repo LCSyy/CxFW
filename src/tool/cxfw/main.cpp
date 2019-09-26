5f705b21e1d01d858ba4865f3a99489be84e32de
LCSyy <18716891497@163.com>
2019-09-26 20:44:03 +0800

    merge

--- src/tool/cxfw/main.cpp
+++ src/tool/cxfw/main.cpp
@@ -4,10 +4,41 @@
 // ...
 
 #include <QCoreApplication>
+#include <QCommandLineParser>
+#include <QCommandLineOption>
+
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    QCoreApplication::setApplicationName("cxfw");
+    QCoreApplication::setApplicationVersion("1.0");
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("CxFW packages manager");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    // parser.addPositionalArgument("command", QCoreApplication::translate("main", "Sub command to run."));
+    // parser.addPositionalArgument("destination", QCoreApplication::translate("main", "Destination directory."));
+
+    QCommandLineOption showProgressOption(QStringList{"i","install"},
+                                          QCoreApplication::translate("main", "Install sdk."),
+                                          QCoreApplication::translate("main", "sdk name"));
+    parser.addOption(showProgressOption);
+
+    QCommandLineOption forceOption(QStringList{"f","force"},
+            QCoreApplication::translate("main", "Overwrite existing files."));
+    parser.addOption(forceOption);
+
+    parser.process(a);
+
+    const QStringList args = parser.positionalArguments();
+    bool force = parser.isSet(forceOption);
+    bool showProgress = parser.isSet(showProgressOption);
+    QString targetDir = parser.value(showProgressOption);
+
+    qDebug() << targetDir;
 
     return a.exec();
 }
