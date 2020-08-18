77a5851e4d63ba77fd5383ae9bb6d85d8d9fec39
chuns <chuns.liu@foxmail.com>
2020-08-18 22:35:47 +0800

    update tool.sh

--- cxfw/tool.sh
+++ cxfw/tool.sh
@@ -1,3 +1,10 @@
-cmake -S . -B "build" -G "MinGW Makefiles"
-cmake --build "build"
-cmake --install "build" --prefix "dist"
+#! /bin/bash
+
+CMAKE_APP=cmake
+if [ ! $(which "cmake" > /dev/null) ]; then
+    CMAKE_APP="D:/Qtz/Tools/CMake_64/bin/cmake.exe"
+fi
+
+$CMAKE_APP -S . -B "build" -G "MinGW Makefiles"
+$CMAKE_APP --build "build"
+$CMAKE_APP --install "build" --prefix "dist"
