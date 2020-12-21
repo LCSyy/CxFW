6b8a62417a6d6a54246020e7a62bba04a58f0d17
chuns <chuns.liu@foxmail.com>
2020-12-21 17:58:49 +0800

    update

--- PIMS/Writer/js/app.js
+++ PIMS/Writer/js/app.js
@@ -22,10 +22,6 @@ Date.prototype.format = function(format) {
     return format;
 }
 
-function isEmptyRow(row) {
-    // ...
-}
-
 // getFirstLine 获取输入文本中的第一行内容
 function getFirstLine(txt) {
     if (txt.length === 0) { return ''; }
@@ -34,11 +30,6 @@ function getFirstLine(txt) {
         return txt;
     }
     return txt.substring(0,lineEndIdx);
-}
-
-// uuid 根据当前时间生成uuid
-function uuid(date) {
-    return date.format('YYYYMMddhhmmssSS')
 }
 
 // db 返回数据库对象
@@ -60,66 +51,6 @@ function initDB() {
                       'create_dt TEXT,'+
                       'update_dt TEXT)');
         tx.executeSql('CREATE TABLE IF NOT EXISTS tags (id INTEGER PRIMARY KEY ASC, name TEXT NOT NULL UNIQUE, title TEXT);');
-    });
-
-    const newVersion = "0.0.3";
-    if(newVersion !== DBVersion) {
-        d.changeVersion(DBVersion, newVersion,function(tx){
-            console.log("upgrade db ...");
-            if (DBVersion === "0.0.1") {
-                tx.executeSql("ALTER TABLE blog ADD COLUMN status TEXT NULL;");
-            } else if (DBVersion === "0.0.2") {
-                tx.executeSql("UPDATE blog SET status = 'release';");
-            }
-            DBVersion = newVersion;
-        });
-    }
-}
-
-// makeRow
-// params:
-//   row: map
-// return:
-//   array<any>
-function makeRow(keys, row) {
-    var vals = [];
-    for (var i in keys) {
-        const key = keys[i];
-        const val = row[key];
-        vals.push(val || null);
-    }
-    return vals;
-}
-
-// insertRow
-function insertRow(sql, keys, row) {
-    console.log("[SQL] " + sql);
-    var d = db();
-    var retId = 0;
-    d.transaction(function(tx){
-        const result = tx.executeSql(sql,makeRow(keys,row));
-        retId = result.insertId;
-    });
-    return retId;
-}
-
-// updateRow
-function updateRow(sql, keys, row) {
-    console.log("[SQL] " + sql);
-    var d = db();
-    if ((row.id || 0) === 0) { throw "[SQL ERROR] UPDATE sql has no id."; }
-
-    d.transaction(function(tx){
-        tx.executeSql(sql,makeRow(keys, row));
-    });
-}
-
-// removeData 删除数据
-function removeData(sql, id) {
-    console.log("[SQL] " + sql);
-    var d = db();
-    d.transaction(function(tx){
-        tx.executeSql(sql, [id]);
     });
 }
 
